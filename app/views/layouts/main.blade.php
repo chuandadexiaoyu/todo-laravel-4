4d062512057f3c5ef2a2039a4ba6e1db599cd190
Shhu <lowscaa@gmail.com>
2013-06-13 09:17:24 +0200

    Update main.blade.php

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -8,8 +8,11 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<script src="/js/app.js"></script>
 </head>
+@if (Auth::check())
 <body style="background-image:url('/img/{{Auth::user()->id}}/background/{{Auth::user()->wallpaper}}')">
-
+@else
+<body>
+@endif
 	@yield('content')
 
 </body>
